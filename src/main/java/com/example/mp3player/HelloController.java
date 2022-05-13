7cf2cd8df76d06ef3aa14940303ea0f53831def8
1ArtAlex <alex.art.0205@mail.ru>
2022-05-13 13:44:41 +0300

    Add files

--- src/main/java/com/example/mp3player/HelloController.java
+++ src/main/java/com/example/mp3player/HelloController.java
@@ -1,24 +1,32 @@
 package com.example.mp3player;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 import javafx.util.Duration;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.Callable;
 
-public class HelloController {
+public class HelloController{
 
     private Timer timer;
     private TimerTask task;
@@ -36,15 +44,21 @@ public class HelloController {
     private Label songLabel;
     @FXML
     private ScrollBar scrollBar;
+    @FXML
+    private HBox allButtons;
+    @FXML
+    private AnchorPane bottomMenu;
+    @FXML
+    private AnchorPane mainWindow;
+    @FXML
+    private AnchorPane sideMenu;
+    @FXML
+    private Label labelCurrentTime;
+    @FXML
+    private Label labelTotalTime;
+    @FXML
+    private Button buttonPPR;
 
-    @FXML
-    private void forwardMedia(ActionEvent actionEvent){
-        mediaPlayer.seek(mediaPlayer.getCurrentTime().add(Duration.seconds(+5)));
-    }
-    @FXML
-    private void backMedia(ActionEvent actionEvent){
-        mediaPlayer.seek(mediaPlayer.getCurrentTime().add(Duration.seconds(-5)));
-    }
 
     @FXML
     private void openMedia(ActionEvent event) {
@@ -60,9 +74,9 @@ public class HelloController {
         int i = subStr.length - 1;
         String name = f.getName();
         System.out.println(name);
-        name = name.replaceAll("%20"," ");
+        name = name.replaceAll("%20", " ");
 
-        if (filePath != null){
+        if (filePath != null) {
             Media media = new Media(filePath);
             mediaPlayer = new MediaPlayer(media);
             mediaPlayer.play();
@@ -95,7 +109,24 @@ public class HelloController {
                     songSlider.setMax(total.toSeconds());
                 }
             });
+
+            volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+                @Override
+                public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+                    mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
+                }
+            });
+
         }
+    }
+
+    @FXML
+    private void forwardMedia(ActionEvent actionEvent) {
+        mediaPlayer.seek(mediaPlayer.getCurrentTime().add(Duration.seconds(+10)));
+    }
+    @FXML
+    private void backMedia(ActionEvent actionEvent) {
+        mediaPlayer.seek(mediaPlayer.getCurrentTime().add(Duration.seconds(-10)));
     }
 
     @FXML
@@ -115,30 +146,22 @@ public class HelloController {
         mediaPlayer.play();
     }
 
-    public void beginTimer(){
+    public void beginTimer() {
         timer = new Timer();
         task = new TimerTask() {
             public void run() {
                 running = true;
                 double current = mediaPlayer.getCurrentTime().toSeconds();
                 double end = mediaPlayer.getTotalDuration().toSeconds();
-                songSlider.setValue(current/end);
-
-                volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
-                        mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
-                    }
-                });
-                if (current/end == 1) {
+                songSlider.setValue(current / end);
+                if (current / end == 1) {
                     cancelTimer();
                 }
             }
         };
-        timer.scheduleAtFixedRate(task, 1000, 1000);
     }
 
-    public void cancelTimer(){
+    public void cancelTimer() {
         running = false;
         timer.cancel();
     }
