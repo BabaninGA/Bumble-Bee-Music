72d9b49573753c1aea78a8990c2920d821e11dc4
Eugene <dedprod.2020@gmail.com>
2022-05-23 10:19:18 +0300

    add new class

---
+++ src/main/java/com/example/mp3player/Playlist.java
@@ -0,0 +1,42 @@
+package com.example.mp3player;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Playlist {
+    String name;
+    //ArrayList<Song> songs;
+
+    public Playlist(){
+        this.name = "";
+        //this.songs=new ArrayList<Song>();
+    }
+    public Playlist(File dir){
+        this.name = dir.getName();
+        File[] song_files = dir.listFiles();
+        //this.songs = new ArrayList<>();
+        for (File f: song_files){
+            if(!f.isDirectory()) {
+                //Song s = new Song(f);
+                //this.songs.add(s);
+            }
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //public ArrayList<Song> getSongs() {
+        //return songs;
+    //}
+
+    //public void setSongs(ArrayList<Song> songs) {
+       // this.songs = songs;
+  //  }
+}
+//}
