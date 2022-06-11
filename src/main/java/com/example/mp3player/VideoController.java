c87f8b8a5b6cd3f0394a94d8fe052225a73be449
1ArtAlex <alex.art.0205@mail.ru>
2022-06-11 09:03:47 +0300

    start of videoplayer

---
+++ src/main/java/com/example/mp3player/VideoController.java
@@ -0,0 +1,99 @@
+package com.example.mp3player;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+public class VideoController {
+
+    @FXML
+    private Button addmp4;
+
+    @FXML
+    private AnchorPane allButtons;
+
+    @FXML
+    private HBox animatedLabel;
+
+    @FXML
+    private Label backMedia;
+
+    @FXML
+    private AnchorPane bottomMenu;
+
+    @FXML
+    private Label forwardMedia;
+
+    @FXML
+    private HBox hboxTime;
+
+    @FXML
+    private HBox hboxVolume;
+
+    @FXML
+    private Label labelButtonPPR;
+
+    @FXML
+    private Label labelCurrentTime;
+
+    @FXML
+    private Label labelRemainingTime;
+
+    @FXML
+    private Label labelTotalTime;
+
+    @FXML
+    private Label labelVolume;
+
+    @FXML
+    private AnchorPane mainWindow;
+
+    @FXML
+    private Label nextSongButton;
+
+    @FXML
+    private Label previousSongButton;
+
+    @FXML
+    private Label shuffleMedia;
+
+    @FXML
+    private AnchorPane sideMenu;
+
+    @FXML
+    private Label songAnimatedLabel;
+
+    @FXML
+    private HBox songHbox;
+
+    @FXML
+    private Label songLabel;
+
+    @FXML
+    private Slider songSlider;
+
+    @FXML
+    private Button switchtoaudio;
+
+    @FXML
+    private Label volumeOff;
+
+    @FXML
+    private Slider volumeSlider;
+    @FXML
+    void addmp4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchtoaudio(ActionEvent event) {
+
+    }
+
+
+}
+
